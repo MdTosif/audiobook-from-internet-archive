e53f6644b5a1d2e34fd7f87b42f7e98252f30b12
mdtosif <md.tosif@zeeve.io>
2025-09-11 00:49:02 +0530

    styling improvement

--- src/pages/audio-playlist/AudioPlaylist.tsx
+++ src/pages/audio-playlist/AudioPlaylist.tsx
@@ -53,13 +53,15 @@ const AudioPlaylist = () => {
       <Show when={audiobook()} fallback={<div>Loading...</div>}>
         {(book) => (
           <div>
-            <div class="flex flex-col md:flex-row gap-8 mb-8">
-              <img
-                src={book().thumbnail}
-                alt={book().title}
-                class="w-full md:w-1/4 rounded-lg shadow-lg"
-              />
-              <div class="flex-1">
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
+              <div class="md:col-span-1">
+                <img
+                  src={book().thumbnail}
+                  alt={book().title}
+                  class="w-full rounded-lg shadow-lg"
+                />
+              </div>
+              <div class="md:col-span-3">
                 <h2 class="text-4xl font-bold mb-2">{book().title}</h2>
                 <p class="text-lg text-muted-foreground mb-4">
                   By {book().creator}
@@ -81,12 +83,12 @@ const AudioPlaylist = () => {
               </div>
             </div>
 
-            <div class="space-y-4">
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
               <For each={book().playlist}>
                 {(track) => (
-                  <div class="bg-secondary p-4 rounded-lg shadow-lg flex items-center justify-between">
-                    <div>
-                      <h3 class="text-lg font-semibold mb-1">
+                  <div class="bg-secondary p-4 rounded-lg shadow-lg flex flex-col items-start justify-between gap-4">
+                    <div class="">
+                      <h3 class="text-lg font-semibold mb-1 truncate">
                         {track.track}. {track.title}
                       </h3>
                       <div class="text-sm text-muted-foreground">
@@ -96,9 +98,8 @@ const AudioPlaylist = () => {
                         </span>
                       </div>
                     </div>
-                    <div class="flex-grow">
-
-                    <AudioPlayer url={track.url} />
+                    <div class="w-full">
+                      <AudioPlayer url={track.url} />
                     </div>
                   </div>
                 )}
